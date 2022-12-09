5b0b693aa8fdc66ecf5cec220ef4cdd6b49f5c64
Yey223 <104279031+Yey223@users.noreply.github.com>
2022-12-09 18:05:55 +0700

    Add files via upload

---
+++ set.js
@@ -0,0 +1,54 @@
+const fs = require('fs')
+const chalk = require('chalk')
+
+global.APIs = {
+	danzz: 'https://danzzapi.xyz',
+}
+
+global.APIKeys = {
+	'https://danzzapi.xyz': '928b0323c9'
+}
+
+// Other
+	global.apikey = ['928b0323c9'] // Get in: https://danzzapi.xyz
+global.apikeyprem = ['928b0323c9'] // Get in: https://danzzapi.xyz
+global.apikeyvip = ['928b0323c9'] // Get in: https://danzzapi.xyz
+global.namabot = ['Tegarbotz']
+global.namaowner = ['Tegars']
+global.owner = ['62895323254598']
+global.premium = ['62895323254598']
+global.packname = '© Powered By'
+global.author = 'https://danzzapi.xyz'
+global.sessionName = 'session'
+global.prefa = ['','!','.','🐦','🐤','🗿']
+global.prefix = ['']
+global.symbol1 = '•'
+global.symbol2 = '>'
+global.wm = '© tegar bot'
+global.mess = {
+    success: 'Success ✓',
+    admin: 'Fitur Khusus Admin Group!',
+    botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!',
+    owner: 'Fitur Khusus Owner Bot',
+    group: 'Fitur Khusus Group Chat',
+    private: 'Fitur Khusus Private Chat!',
+    bot: 'Fitur Khusus Nomor Bot',
+    wait: 'Waittt...',
+    notregist: 'Kamu belum terdaftar di database bot silahkan daftar terlebih dahulu',
+    premium: 'Kamu Bukan User Premium, Beli Sana Ke Owner Bot',
+    endLimit: 'Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Pukul 00:00 WIB.',
+}
+global.limitawal = {
+	free: "3",
+        premium: "unlimited"
+}
+global.thumb = fs.readFileSync('./media/img/1.jpg')
+global.qris = { url: 'https://telegra.ph/file/cd0372ebf13b265f3feb5.jpg' }
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.greenBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
